11ac36c44f80300c3178e9c646c8081abe3acd8a
echetoui <56730663+echetoui@users.noreply.github.com>
2026-04-05 16:09:38 -0400

    chore: export Alert from design-system index

--- frontend/src/design-system/index.js
+++ frontend/src/design-system/index.js
@@ -2,8 +2,8 @@
 export { Button } from './Button';
 export { Card } from './Card';
 export { Input } from './Input';
+export { Alert } from './Alert';
 export { ThemeToggle } from './ThemeToggle';
-// Alert, Badge, and Section exports will be added once components are created
-// export { Alert } from './Alert';
+// Badge and Section exports will be added once components are created
 // export { Badge } from './Badge';
 // export { Section } from './Section';
